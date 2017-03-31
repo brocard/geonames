752cde2972870fde81a7aaef681d6b63fdef46e8
Yusniel Brocard <brocard@gmail.com>
2017-03-31 19:32:25 +0000

    Apply fixes from StyleCI

--- src/database/migrations/2016_12_30_125344_add_country_id_as_foreign_key_on_states_table.php
+++ src/database/migrations/2016_12_30_125344_add_country_id_as_foreign_key_on_states_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddCountryIdAsForeignKeyOnStatesTable extends Migration
 {
@@ -14,11 +14,9 @@ class AddCountryIdAsForeignKeyOnStatesTable extends Migration
     public function up()
     {
         Schema::table('states', function (Blueprint $table) {
-
             $table->foreign('country_id')
                 ->references('id')->on('countries')
                 ->onDelete('cascade');
-
         });
     }
 
